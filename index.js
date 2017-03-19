b188e37ff48a83406ce72b17dde134f850ab0bf9
Krzysztof Słowik <slowik.krzysiek+git@gmail.com>
2017-03-19 18:54:32 +0100

    * fix spaces between 'data-uri(  path_to_file  )'

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-var postcss = require('postcss');
-var mime = require('mime');
-var fs = require('fs');
+var postcss = require('postcss'),
+    mime = require('mime'),
+    fs = require('fs');
 
 module.exports = postcss.plugin('postcss-data-uri', function (opts) {
-    opts = opts || {};
     return function (css) {
         css.walkDecls(/background(\-image)?/i, function (decl) {
-            var rePattern = /data-uri\(['|"]?(.*?)['|"]?\)/i;
+            var rePattern = /data-uri\(\s*['|"]?(.*?)['|"]?\s*\)/i,
+                path;
 
             if (rePattern.test(decl.value)) {
-                var path = decl.value.match(rePattern)[1].trim();
+                path = decl.value.match(rePattern)[1];
                 if (fs.existsSync(path)) {
                     decl.value = decl.value.replace(rePattern, 'url(data:' + mime.lookup(path) + ';base64,' + new Buffer(fs.readFileSync(path)).toString('base64') + ')');
                 } else {
-                    console.log('Image ',path,' not foud.')
-                    // decl.prop = 'background-image';
+                    console.log('Image ',path,' not found.')
                 }
             }
         });
